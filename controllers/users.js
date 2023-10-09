9bb93a7ee56b026439a84b759458f1b396fca70c
decode <isc20350293@gmail.com>
2023-10-09 13:15:53 -0600

    Consulta a la bd

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,19 @@
 const {request, response} = require('express');
+const conn = require('../db');
 
 const listUsers = (req = request, res = response) => {
-    res.json({msg:"Hola usuario, llevame con tu lider..."})
-}
+    try {
+        conn.query('SELECT * FROM backend', (err, result) => {
+            if (err) {
+                throw err
+            }
+            res.json(result);
+        });
+    } catch (error) {
+            res.status(500).json(error);
+        } finally {
+        conn.end();
+        }
+    }
 
 module.exports = {listUsers};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 const {Router} = require('express');
+
 const {listUsers} = require('../controllers/users');
 
 const router = Router();
 
 //http://localhost:3000/api/v1/users/
 router.get('/', listUsers);
+//router.post('/', listUsers);
+//router.put('/', listUsers);
+//router.patch('/', listUsers);
+//router.delete('/', listUsers);
+
 
 module.exports = router;
